5ea175a4fe98cfbe1608dc0b34dd44efe20a4c2f
Simon van der Veldt <simon.vanderveldt@gmail.com>
2014-02-06 19:32:41 +0100

    Config: Set timezone to Europe/Amsterdam Solves nikola deploy throwing an error, fixes #17 [deploy]

--- conf.py
+++ conf.py
@@ -595,7 +595,7 @@ INDEX_DISPLAY_POST_COUNT = 10
 # Also, if you want to use a different timezone in some of your posts,
 # you can use W3C-DTF Format (ex. 2012-03-30T23:00:00+02:00)
 #
-# TIMEZONE = 'Europe/Zurich'
+TIMEZONE = 'Europe/Amsterdam'
 
 # If webassets is installed, bundle JS and CSS to make site loading faster
 # USE_BUNDLES = True
